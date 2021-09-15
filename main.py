a5d30437fa63704a3e60cc2b5056064c207b1dce
Elzhan <47031370+Elzhan4239@users.noreply.github.com>
2021-09-15 20:13:05 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+from kivy.core.window import Window
+
+Window.size=(300,100)
+Window.clearcolor=(255/255, 186/255, 3/255,1)
+Window.title = "Приложение"
+
+
+class MyApp(App):
+
+    def build(self):
+        box = BoxLayout()
+        btn = Button(text="Нажми на меня")
+        label = Label (text='Моя программа')
+        box.add_widget(label)
+        box.add_widget(btn)
+        return box
+
+if __name__=="__main__":
+     MyApp().run()
